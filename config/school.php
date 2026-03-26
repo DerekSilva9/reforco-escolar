a08f5619b0f6de83e0bdff8c379ff187be8d0aa2
DerekSilva <88068277+DerekSilva9@users.noreply.github.com>
2026-03-26 18:16:54 -0300

    Landing Page

---
+++ config/school.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'address' => env('SCHOOL_ADDRESS'),
+    'whatsapp' => env('SCHOOL_WHATSAPP'),
+    'email' => env('SCHOOL_EMAIL'),
+    'maps_url' => env('SCHOOL_MAPS_URL'),
+];
+
